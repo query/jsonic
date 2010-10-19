c228804657015f078cfb0e6b4595a1917efae0cf
Roger Que <alerante@bellsouth.net>
2010-10-19 19:34:49 +0800

    Add rudimentary support for synthesis through Mac OS X's `say`
    Rate and pitch alterations are not supported.  PyObjC is required in
    order to look up system voices through NSSpeechSynthesizer.

--- server/synthesizer.py
+++ server/synthesizer.py
@@ -13,6 +13,8 @@ import hashlib
 import itertools
 import os
 
+from AppKit import NSSpeechSynthesizer
+
 class SynthesizerError(Exception): 
     '''
     Exception to throw for any synthesis error, including a human readable
@@ -189,9 +191,65 @@ class EspeakSynth(ISynthesizer):
             }
         return cls.INFO
 
+class MacOSXSpeechSynth(ISynthesizer):
+    '''
+    Synthesizes speech using the Mac OS X `say` command (10.6 or later).
+    
+    :ivar _path: Output cache path
+    :ivar _opts: Command line options for `say`
+    :cvar INFO: Dictionary of all supported engine properties cached for 
+        fast responses to queries
+    '''
+    INFO = None
+    
+    def __init__(self, path, properties):
+        '''Implements ISynthesizer constructor.'''
+        # path where to write the file
+        self._path = path
+        # NSSpeechSynthesizer options for this synth instance
+        self._opts = []
+        
+        try:
+            voice = str(properties['voice'])
+            assert voice in MacOSXSpeechSynth.get_info()['voices']['values']
+            self._opts.append(voice)
+        except AssertionError:
+            raise SynthesizerError('invalid voice')
+        except KeyError:
+            self._opts.append('Alex')
+
+        # store property portion of filename
+        self._optHash = hashlib.sha1('macosx' + str(self._opts)).hexdigest()
+
+    def write_wav(self, utterance):
+        '''Implements ISynthesizer.write_wav.'''
+        utf8Utterance = utterance.encode('utf-8')
+        utterHash = hashlib.sha1(utf8Utterance).hexdigest()
+        hashFn = '%s-%s' % (utterHash, self._optHash)
+        # write wave file into path
+        wav = os.path.join(self._path, hashFn+'.wav')
+        if not os.path.isfile(wav):
+            args = self._opts + [wav]
+            c = iterpipes.cmd('say --file-format=WAVE --data-format=LEI16 -v {} -o {}', *args, encoding='utf-8')
+            ret = iterpipes.call(c, utterance)
+        return hashFn
+
+    @classmethod
+    def get_info(cls):
+        '''Implements ISynthesizer.get_info.'''
+        if cls.INFO is None:
+            voices = NSSpeechSynthesizer.availableVoices()
+            cls.INFO = {
+                'voices' : {
+                    'values' : [x.split('.')[-1] for x in voices],
+                    'default' : 'Alex'
+                }
+            }
+        return cls.INFO
+
 # global list of available synth implementations
 # @todo: add these dynamically if the synths actually work on the platform
-SYNTHS = {'espeak' : EspeakSynth}
+SYNTHS = {'espeak' : EspeakSynth, 'macosx' : MacOSXSpeechSynth}
 
 def get_class(name):
     '''
